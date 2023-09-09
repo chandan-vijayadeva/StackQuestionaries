ca91c394ff8788a9e1b42647a6e2e22201435638
Chandan V <51235527+chandan-vijayadeva@users.noreply.github.com>
2023-09-09 20:48:56 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import StackScreen from './src/screens/StackScreen';
+
+const App = () => (
+  <SafeAreaView style={{flex: 1}} backgroundColor={'Black'}>
+    <StackScreen />
+  </SafeAreaView>
+);
+export default App;
